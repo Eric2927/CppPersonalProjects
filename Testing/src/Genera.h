a4a2a77323f4de3188d593845876448003e21c55
EricLi <eric2927.li@gmail.com>
2019-10-10 20:10:35 -0400

    picoCTF things

---
+++ Testing/src/Genera.h
@@ -0,0 +1,20 @@
+#ifndef GENERA_H_
+#define GENERA_H_
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <vector>
+#include <initializer_list>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <algorithm>
+#include <array>
+#include <cmath>
+using namespace std;
+
+
+#endif /* GENERA_H_ */
